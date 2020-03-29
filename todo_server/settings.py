7ea4e647da1e80abf101ee525f8bfe487d84dfa3
Peiyu <1206471828@qq.com>
2020-03-29 21:55:50 +0800

    edit settings.py

--- todo_server/settings.py
+++ todo_server/settings.py
@@ -126,26 +126,3 @@ STATIC_URL = '/static/'
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ()
-
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-    'VIEW',
-)
-
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
